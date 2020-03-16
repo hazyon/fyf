a1f5c71fd8b16b9f07a5747f0f24aac30140a98a
Tanvi Ganapathy <tanvig22@lakesideschool.org>
2020-03-16 16:47:03 -0700

    connect Study Buddy App to Firebase

--- study_buddy/lib/home.dart
+++ study_buddy/lib/home.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(25),
-      child: Text(
-        "Home",
-        style: TextStyle(fontSize: 36),
-      ),
-    );
+      child: Column(
+        children: <Widget>[
+        Text("Home", style: TextStyle(fontSize: 36),),
+      FlatButton(
+      child: Text('Add'),
+      onPressed: () {
+
+      Firestore.instance
+          .collection('tasks')
+          .add({
+            "title": "new meeting",
+            "description": "at a specific time"
+          })
+          .catchError((err) => print(err));
+      })]
+    ));
   }
 }
 
